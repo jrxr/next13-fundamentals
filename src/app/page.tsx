5a18e67de00b9d22e086587c43cf049bc2f2af97
agacy junior <juniormartinssom@gmail.com>
2023-10-19 19:48:43 -0300

    Data Fetch in Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-import Image from 'next/image'
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
 
-export default function Home() {
-  return <h1>Hello World</h1>
+  const response = await fetch('https://api.github.com/users/diego3g')
+  const user = await response.json()
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
